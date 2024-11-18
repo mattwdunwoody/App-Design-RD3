c9a76b456fc29e1ec0968ee212826c03c983de10
mattwdunwoody <widmatg@dunwoody.edu>
2024-11-18 13:48:59 -0600

    first commit

---
+++ src/abstractTest1Pack/ABC.java
@@ -0,0 +1,11 @@
+package abstractTest1Pack;
+
+public abstract class ABC {
+	ABC() {
+		System.out.println("Initiliazing ABC class");
+	}
+	void FOO() {
+		System.out.println("FOO");
+	}
+	abstract String DAY();
+}
